10fe09f5bff294e59c7bd0039d9407b7ca074e1b
dileep <dileep20721@gmail.com>
2024-03-05 18:19:05 +0530

    Add project files.

---
+++ exception/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+namespace exception
+
+{
+    class Myexception : Exception
+    {
+        public Myexception(string message):base(message)
+        {
+            //Console.WriteLine(message);
+        }
+    }
+    class exceptions
+    {
+        public static void method()
+        {
+            int i = 100;
+            try
+            {
+                int x = i/0;
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 0;
+            try
+            {
+                int z = x / y;
+                // Console.WriteLine("hello");
+                //throw new DivideByZeroException("Exceptionnnnn");
+                //exceptions.method();
+                //throw new Myexception("UserDefined Exception");
+            }
+            catch (Myexception e)
+            {
+                Console.WriteLine("Exception occured ");
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e);
+            }
+            //catch
+            //{
+            //    Console.WriteLine("Exception");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("finally");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
